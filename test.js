8bdb4e18e78ba4d78eb4066f67d6a7a20ce32dab
LeWilSy <b.tighidet0@gmail.com>
2024-01-02 19:28:58 +0100

    ADD KEY EVENT / RESPONSIVE+UX VISUAL / FIX JS CALC

--- test.js
+++ test.js
@@ -1,55 +1,3 @@
-// Declare all global variables
-
-const mouseCircle = document.getElementById("mouse");
-calcContainer = document.getElementById("calc-container");
-resContainer = document.getElementById("res-container");
-calcKey = document.querySelectorAll("td");
-tableCalcKey = Array.from(calcKey);
-calc = document.querySelector("table");
-resultat = document.getElementById("spanres");
-operators = ["+", "-", "/", "*"]; // mathematic operators array
-
-let resBorderIsBlue = false; // Boolean who become true only if res-container's border is blue
-let total = "0"; // initialize total as same value of inner html of spanres
-let target; // initialize target in global variables so we can use it in all event listner
-let startTimer; // start timer between mouse down and mouse up
-let endTimer; // end timer between mouse down and mouse up
-
-// Event listener for change border in fonction of user's selection, increasing UX
-window.addEventListener("mousedown", (e) => {
-  // event on window, to allow user to unselect the calculator
-  if (
-    e.target.offsetParent == resContainer ||
-    e.target.offsetParent == calc ||
-    e.target == resContainer ||
-    e.target == calc
-  ) {
-    resContainer.style.border = "2px solid #81a7da";
-    resBorderIsBlue = true;
-  } else {
-    resContainer.style.border = "2px solid white";
-    resBorderIsBlue = false;
-  }
-});
-
-// event listener for hover on res-container
-resContainer.addEventListener("mouseenter", () => {
-  resContainer.style.background = "#dedde0c0";
-  if (resBorderIsBlue == false) {
-    // if res-container's border is blue, it doesnt change until user unselect the calculator
-    resContainer.style.border = "none";
-  }
-});
-
-// event listener for hover on res-container
-resContainer.addEventListener("mouseleave", () => {
-  resContainer.style.background = "#c0bfc2c0";
-  if (resBorderIsBlue == false) {
-    // if res-container's border is blue, it doesnt change until user unselect the calculator
-    resContainer.style.border = "2px solid white";
-  }
-});
-
 window.addEventListener("keydown", (e) => {
   let lastCar = total.slice(-1);
   target = e.key;
@@ -96,7 +44,7 @@ window.addEventListener("keydown", (e) => {
   }
   if (target === "Backspace") {
     total = total.slice(0, -1);
-    if (total == 0) {
+    if (total === "0") {
       total = "0";
     }
   }
@@ -146,7 +94,7 @@ calc.addEventListener("mouseup", function chiffres(e) {
     total += target;
   } else if (target === "C") {
     total = total.slice(0, -1);
-    if (total == 0) {
+    if (total === "0") {
       total = "0";
     }
     if (timer > 1000) {
@@ -158,44 +106,3 @@ calc.addEventListener("mouseup", function chiffres(e) {
   }
   resultat.innerHTML = total;
 });
-
-// MOUSE CIRCLE
-
-window.addEventListener("mousemove", (e) => {
-  mouseCircle.style.left = e.pageX + "px";
-  mouseCircle.style.top = e.pageY + "px";
-  if (
-    e.target == resultat ||
-    e.target == resContainer ||
-    e.target.offsetParent == calc ||
-    e.target == calc ||
-    e.target == calcContainer
-  ) {
-    mouseCircle.style.opacity = "0";
-  } else {
-    mouseCircle.style.opacity = "1";
-  }
-});
-
-// *************************************** TESTING THINGS *************************************************
-
-// function add(x) {
-//   total += x;
-//   return total;
-// }
-// function subs(x) {
-//   total -= x;
-//   return total;
-// }
-// function div(x) {
-//   if (x === 0) {
-//     total = "ERROR";
-//   } else {
-//     total /= x;
-//     return total;
-//   }
-// }
-// function mult(x) {
-//   total *= x;
-//   return total;
-// }
